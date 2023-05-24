854cf8869aeb351180a909b98d8fa815b92ccd46
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-05-24 02:26:21 -0300

    Revert "Revert "Avances de proyecto, storage, utils, middlewares. Faltan inplementaciones""
    This reverts commit ebab6c57f0b78a9ed10ba4d894b498e2af6af30c.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
+const path = require("path");
 
-const PATH_ROUTES = __dirname; //ruta absoluda donde se encuentra este doc
+const PATH_ROUTES = __dirname; //ruta absoluta donde se encuentra este doc
+console.log(PATH_ROUTES);
 
-const removeExtension = (fileName) => {
-  return fileName.split(".").shift();
-};
+// const removeExtension = (fileName) => {
+//   return fileName.split(".").shift();
+// };
 
 fs.readdirSync(PATH_ROUTES).filter((file) => {
-  const name = removeExtension(file);
+  const name = path.parse(file).name; // usa el módulo de node PATH, testear
   if (name !== "index") {
     router.use(`/${name}`, require(`./${file}`));
   }
